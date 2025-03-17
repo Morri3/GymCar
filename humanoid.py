604ded057f8aa2d2c4eeb4f1e8d50e42f5be37d3
Morri3 <824440291@qq.com>
2025-03-17 09:32:38 +0800

    fit: 添加多训练轮次，计算score

--- humanoid.py
+++ humanoid.py
@@ -2,23 +2,46 @@ import gymnasium as gym
 
 # 1. Initialise the environment
 env = gym.make('HumanoidStandup-v5', impact_cost_weight=0.5e-6, \
-    render_mode="human", width=1920, height=1280)
-# 2. Reset the environment to generate the first observation and get extra info
-observation, info = env.reset()
-episode_over = False
-
-while not episode_over:
-    # 1) Agent policy that uses the observation and info
-    #   ( this is where you would insert your policy )
-    action = env.action_space.sample() # here is the random action
-    
-    # 2) Step (transition) through the environment with the action
-    #   receiving the next observation, reward and if the episode has terminated
-    #   terminated: bot has finished tasks or bot has been destroyed → stop the environment
-    #   truncated: after a fixed number of timesteps → stop the environment
-    observation, reward, terminated, truncated, info = env.step(action)
-
-    # 3) Determine whether the episode is over
-    episode_over = terminated or truncated
-# 3. Close the environment
+    render_mode="human", width=1280, height=1024)
+# 2. epoches of episodes
+episode_epoches = 10
+# 3. traverse each epoch
+for epoch in range(1, episode_epoches + 1):
+    # 1) Reset the environment to generate the first observation and get extra info
+    observation, info = env.reset()
+    # 2) initialise variables
+    episode_over = False
+    score = 0
+    # 3) do something
+    while not episode_over:
+        # Agent policy that uses the observation and info
+        #   ( this is where you would insert your policy )
+        action = env.action_space.sample() # here is the random action
+        # Step (transition) through the environment with the action
+        #   receiving the next observation, reward and if the episode has terminated
+        #   terminated: bot has finished tasks or bot has been destroyed → stop the environment
+        #   truncated: after a fixed number of timesteps → stop the environment
+        observation, reward, terminated, truncated, info = env.step(action)
+        # Determine whether the episode is over
+        episode_over = terminated or truncated
+        # update overall score        
+        score += reward
+    # 4) output the score of eaach epoch
+    print("Episode: ", epoch, "; score: ", score)
+# 4. Close the environment
 env.close()
+
+
+# while not episode_over:
+#     # 1) Agent policy that uses the observation and info
+#     #   ( this is where you would insert your policy )
+#     action = env.action_space.sample() # here is the random action
+    
+#     # 2) Step (transition) through the environment with the action
+#     #   receiving the next observation, reward and if the episode has terminated
+#     #   terminated: bot has finished tasks or bot has been destroyed → stop the environment
+#     #   truncated: after a fixed number of timesteps → stop the environment
+#     observation, reward, terminated, truncated, info = env.step(action)
+
+#     # 3) Determine whether the episode is over
+#     episode_over = terminated or truncated
